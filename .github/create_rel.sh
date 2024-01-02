b31c8e9d34f5c75e439b4eac855aa250182db30e
Matt Gathu <mattgathu@gmail.com>
2024-01-02 08:18:39 +0100

    Add permissions

--- .github/create_rel.sh
+++ .github/create_rel.sh
@@ -10,7 +10,7 @@ ghv=$(gh release list -L 1 | awk -F" " '{print $1}')
 if [[ $pkgv != $ghv ]]; then
     echo "'$pkgv' != '$ghv' ... creating new release tag"
     # create new rel tag
-    $(gh release create "$pkgv"  --repo="$GITHUB_REPOSITORY" --title="$pkgv" --generate-notes)
+    $(gh release create "$pkgv"  --repo="$GITHUB_REPOSITORY" --title="$pkgv" --target="$GITHUB_SHA" --generate-notes)
 else
     echo "$pkgv == $ghv ... stopping"
 fi

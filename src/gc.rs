7bb45720ac0ef4e094ed121de9245d20b1ed7c68
Matt Gathu <mattgathu@gmail.com>
2024-03-04 07:56:06 +0100

    Fmt

--- src/gc.rs
+++ src/gc.rs
@@ -1,10 +1,10 @@
 use std::path::Path;
 
 use crate::{
-    core::VlogNum,
-    error::GhalaDbResult,
-    keys::Keys,
-    vlog::{DataEntry, VlogReader},
+  core::VlogNum,
+  error::GhalaDbResult,
+  keys::Keys,
+  vlog::{DataEntry, VlogReader},
 };
 
 /// A Lightweight Garbage Collector
@@ -33,39 +33,39 @@ use crate::{
 /// the entire vlog. This could be problematic.
 /// TODO: handle this edge case
 pub(crate) struct GarbageCollector {
-    vnum: VlogNum,
-    vlog_iter: VlogReader,
+  vnum: VlogNum,
+  vlog_iter: VlogReader,
 }
 
 impl GarbageCollector {
-    pub fn new(vnum: VlogNum, path: &Path) -> GhalaDbResult<Self> {
-        debug!("GarbageCollector::new vlog: {vnum} at: {path:?}");
-        let vlog_iter = VlogReader::from_path(path)?;
-        Ok(Self { vnum, vlog_iter })
-    }
+  pub fn new(vnum: VlogNum, path: &Path) -> GhalaDbResult<Self> {
+    debug!("GarbageCollector::new vlog: {vnum} at: {path:?}");
+    let vlog_iter = VlogReader::from_path(path)?;
+    Ok(Self { vnum, vlog_iter })
+  }
 
-    pub fn sweep(&mut self, keys: &mut Keys) -> GhalaDbResult<Option<DataEntry>> {
-        trace!("GarbageCollector::sweep");
-        loop {
-            match self.vlog_iter.next_entry()? {
-                None => return Ok(None),
-                Some((dp, de)) => {
-                    match keys.get(&de.key) {
-                        None => continue,
-                        Some(cur_dp) => {
-                            if cur_dp == dp {
-                                // data is live and should move to tail
-                                return Ok(Some(de));
-                            } else {
-                                continue;
-                            }
-                        }
-                    }
-                }
+  pub fn sweep(&mut self, keys: &mut Keys) -> GhalaDbResult<Option<DataEntry>> {
+    trace!("GarbageCollector::sweep");
+    loop {
+      match self.vlog_iter.next_entry()? {
+        None => return Ok(None),
+        Some((dp, de)) => {
+          match keys.get(&de.key) {
+            None => continue,
+            Some(cur_dp) => {
+              if cur_dp == dp {
+                // data is live and should move to tail
+                return Ok(Some(de));
+              } else {
+                continue;
+              }
             }
+          }
         }
+      }
     }
-    pub fn vnum(&self) -> VlogNum {
-        self.vnum
-    }
+  }
+  pub fn vnum(&self) -> VlogNum {
+    self.vnum
+  }
 }
